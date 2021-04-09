8606a0c61514935ef7c3f113e943c589b0171b54
Roni Serra <roniblizz87@gmail.com>
2021-04-09 20:50:45 -0300

    Desafio ok :tada:!!

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
+import { useMovies } from '../hooks/useMovies'
+import { MovieCard } from './MovieCard'
+
+import '../styles/content.scss'
+
 export function Content() {
-  // Complete aqui
+  const { movies, selectedGenre } = useMovies()
+
+  return (
+    <div className='container'>
+      <header>
+        <span className='category'>
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className='movies-list'>
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
+import { useMovies } from '../hooks/useMovies'
+import { Button } from './Button'
+
+import '../styles/sidebar.scss'
+
 export function SideBar() {
-  // Complete aqui
+  const { genres, selectedGenreId, setSelectedGenreId } = useMovies()
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id)
+  }
+
+  return (
+    <nav className='sidebar'>
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className='buttons-container'>
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
+
